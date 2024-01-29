c794dc23f6e124c1a4dd1d26bcd53b7c37415450
luis.ignacio <luis.ignacio@grupotimexico.com>
2024-01-29 17:23:02 -0600

    hola2

--- pet/src/main/java/org/bedu/java/backend/pet/PetApplication.java
+++ pet/src/main/java/org/bedu/java/backend/pet/PetApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetApplication.class, args);
+		SpringApplic ation.run(PetApplication.class, args);
 a
 	}
 	
