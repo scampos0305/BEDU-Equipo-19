d8fd22bf23d27eb4c0836f3fced589a14ada4ee3
luis.ignacio <luis.ignacio@grupotimexico.com>
2024-01-29 17:14:27 -0600

    hola

--- pet/src/main/java/org/bedu/java/backend/pet/PetApplication.java
+++ pet/src/main/java/org/bedu/java/backend/pet/PetApplication.java
@@ -8,7 +8,7 @@ public class PetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetApplication.class, args);
-		string()
+a
 	}
-	publi static clase hola(){}
+	
 }
